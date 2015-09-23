60ac662a958a0a5b521d066cf568d34231be0395
Philip Dai <smartielion@gmail.com>
2015-09-23 14:54:21 -0500

    Using templateUrl to replace template property in my customized directive

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ myApp.controller('secondController', ['$scope', '$log', '$routeParams', function
 myApp.directive('searchResult', function() {
 	return {
 		restrict: 'EACM',
-		template: '<a href="#" class="list-group-item"><h4 class="list-group-item-heading">Doe, John</h4><p class="list-group-item-text">555 Main St., New York, NY 11111</p></a>',
+		templateUrl: 'directives/searchresult.html',
 		replace: true
 	};
 });
