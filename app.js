439d6464987cf5765d21f2b2f5278bd55dc14e89
Philip Dai <smartielion@gmail.com>
2015-09-23 11:30:34 -0500

    Single page and hash demo

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var myApp = angular.module('myApp', []);
-
-myApp.controller('mainController', ['$scope', function($scope) {
-    
-  $scope.name = "Main";  
-    
-}]);
-
-myApp.controller('secondController', ['$scope', function($scope) {
-    
-  $scope.name = "Second";  
-    
-}]);
+window.addEventListener('hashchange', function() {
+	if (window.location.hash === '#bookmark/1') {
+		console.log('Page 1 is cool.');
+	}
+	
+	if (window.location.hash === '#bookmark/2') {
+		console.log('Page 2 is cool.');
+	}
+	
+	if (window.location.hash === '#bookmark/3') {
+		console.log('Page 3 is cool.');
+	}
+					
+});
