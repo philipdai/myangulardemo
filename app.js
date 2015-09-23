8f4748f4b9c174210b7b43aa73ed885fc179e5be
Philip Dai <smartielion@gmail.com>
2015-09-23 01:11:05 -0500

    Interpolation demo

--- app.js
+++ app.js
@@ -2,18 +2,22 @@
 var myApp = angular.module('myApp', ['ngMessages', 'ngResource']);
 
 // CONTROLLERS
-myApp.controller('mainController', ['$scope', '$log', '$filter', '$resource', function($scope, $log, $filter, $resource) {
+myApp.controller('mainController', ['$scope', '$timeout', '$log', '$filter', '$resource', function($scope, $timeout, $log, $filter, $resource) {
 	$log.log("This is some log.");
 	$log.info("This is some info.");
 	$log.warn("This is some warning!");
 	$log.debug("This is some debug info.");
 	$log.error("This is some error infor!");
 	
-	$scope.name = "John";
+	$scope.name = "Philip Dai";
 	$scope.formattedName = $filter('uppercase')($scope.name);
 	$log.log("The name is " + $scope.name);
 	$log.log("The formatted name is " + $scope.formattedName);
 	
 	$log.log($resource);
+	
+	$timeout(function() {
+		$scope.name = "Everybody";
+	}, 3000);
 }]);
 
